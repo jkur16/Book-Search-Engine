a464f58654381d0415be136d95168980a57b5adb
Jamie Kuras <jamiekuras@gmail.com>
2021-09-04 15:25:23 -0400

    Added login to mutation in resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,26 @@
 const { Book, User } = require('../models');
 
-const resolvers 
+const resolvers ={
+    Query: {
+        me: async ()
+    },
+
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const profile = await Profile.findOne({ email });
+      
+            if (!profile) {
+              throw new AuthenticationError('No profile with this email found!');
+            }
+      
+            const correctPw = await profile.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect password!');
+            }
+      
+            const token = signToken(profile);
+            return { token, profile };
+          },
+    }
+}
