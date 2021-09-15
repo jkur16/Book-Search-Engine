838263cf40e4707e7b2ffc87e1dbcc095ed54de4
Jamie Kuras <jamiekuras@gmail.com>
2021-09-15 13:31:49 -0400

    added useMutations, useQuery to SavedBooks

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -6,13 +6,14 @@ import { REMOVE_BOOK } from '../utils/mutations'
 import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
+import { useMutation, useQuery } from '.@apollo/client';
 
 const SavedBooks = () => {
   const { loading, data } = useQuery(GET_ME);
-  const userData = data?.me || [];
+  const [removeBook, {error}] = useMutation(REMOVE_BOOK)
 
   // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
+  const userData = data?.me || []
 
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
@@ -25,15 +26,12 @@ const SavedBooks = () => {
 
     try {
       const response = await deleteBook(bookId, token);
+      const {data} = await removeBook({
+        variables: {bookId: bookId}
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
-      // upon success, remove book's id from localStorage
       removeBookId(bookId);
+
     } catch (err) {
       console.error(err);
     }
